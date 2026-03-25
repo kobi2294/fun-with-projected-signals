b0e994d94a3aa5b9460af2ccd17929917323d5cc
Kobi Hari <kobi2294@yahoo.com>
2026-03-25 18:47:49 +0200

    withResources

--- src/app/store/with-resource-demo/with-resource-demo.slice.ts
+++ src/app/store/with-resource-demo/with-resource-demo.slice.ts
@@ -2,10 +2,12 @@ import { ContactType } from "./models";
 
 export interface WithResourceDemoSlice {
     readonly userId: number | null;    
+    readonly isLoadingUserContacts: boolean;
     readonly contactDetails: Partial<Record<ContactType, string>>;
 }
 
 export const initialWithResourceDemoSlice: WithResourceDemoSlice = {
+    isLoadingUserContacts: false,
     userId: null,
     contactDetails: {}
 }
--- src/app/store/with-resource-demo/with-resource-demo.store.ts
+++ src/app/store/with-resource-demo/with-resource-demo.store.ts
@@ -1,30 +1,32 @@
-import { updateState, withDevtools, withResource } from "@angular-architects/ngrx-toolkit";
-import { signalStore, withState } from "@ngrx/signals";
-import { initialWithResourceDemoSlice } from "./with-resource-demo.slice";
-import { withResources } from "../../lib/with-resources/with-resource";
-import { delegateResource } from "../../lib/with-resources/delegate-resource";
-import { resource } from "@angular/core";
-import { getContactEntriesForUser } from "./api";
+import { updateState, withDevtools, withResource } from '@angular-architects/ngrx-toolkit';
+import { signalStore, withState } from '@ngrx/signals';
+import { initialWithResourceDemoSlice } from './with-resource-demo.slice';
+import { withResources } from '../../lib/with-resources/with-resource';
+import { delegateResource } from '../../lib/with-resources/delegate-resource';
+import { resource } from '@angular/core';
+import { getContactEntriesForUser } from './api';
 
 export const WithResourceDemoStore = signalStore(
-    {providedIn: 'root'},
-    withState(initialWithResourceDemoSlice), 
-    withDevtools('With Resource Demo Store'), 
-    withResources(store => ({
-        userContacts: delegateResource({
-            resource: resource({
-                params: () => store.userId(),
-                loader: req => getContactEntriesForUser(req.params),
-            }), 
-            updater: (snapshot) => {
-                if (snapshot.status === 'resolved' && snapshot.value) {
-                    const contacts = snapshot.value.contacts.map(entry => [entry.type, entry.value] as const);
-                    const details = Object.fromEntries(contacts);
-
-                    updateState(store, 'userContactDetails', details)
-
-                }
-            }
-        })
-    }))
-)
+  { providedIn: 'root' },
+  withState(initialWithResourceDemoSlice),
+  withDevtools('With Resource Demo Store'),
+  withResources((store) => ({
+    userContacts: delegateResource({
+      resource: resource({
+        params: () => store.userId(),
+        loader: (req) => getContactEntriesForUser(req.params),
+      }),
+      updater: (snapshot) => {
+        if (snapshot.status === 'resolved' && snapshot.value) {
+          const contacts = snapshot.value.contacts.map(
+            (entry) => [entry.type, entry.value] as const,
+          );
+          const details = Object.fromEntries(contacts);
+          updateState(store, 'userContactDetails', details, { isLoadingUserContacts: false });
+        } else if (snapshot.status === 'loading') {
+          updateState(store, 'isLoadingUserContacts', { isLoadingUserContacts: true });
+        }
+      },
+    }),
+  })),
+);
